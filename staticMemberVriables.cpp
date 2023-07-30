146bd9e5e55db89154d122536918d6899ea2d141
Himanshu Raj Aman <himanshu.aman@samarth.ac.in>
2023-07-30 20:10:04 +0530

    OOPS Static member variables

---
+++ staticMemberVriables.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+
+/**
+ * Static member variable are those who are independent of the instance of class
+ *  - They can be independently called/invoked without actually creating any
+ * instance of the class
+ *  - They occupy memory on the first instance of the class
+ *  - Lifetime: throughout the program
+ *  - Default value: 0
+ *  - They are also known as Class Member Variables
+ */
+class Amount {
+private:
+  int x; //Instance member variable
+
+public:
+  static float roi; //Static member variable / Class member variable
+};
+
+float Amount::roi; //Defining static member variable *important
+
+int main() {
+
+  std::cout << Amount::roi;
+
+  Amount a;
+
+  Amount::roi = 4.5f;
+
+  std::cout << a.roi;
+}
