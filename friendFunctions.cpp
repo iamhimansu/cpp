6d3ffa2c9f8bb36ea9046b06351336f3388ba5ef
Himanshu Raj Aman <himanshu.aman@samarth.ac.in>
2023-08-01 12:26:05 +0530

    Friend function

--- friendFunctions.cpp
+++ friendFunctions.cpp
@@ -1,3 +1,28 @@
-//
-// Created by iamhi on 01-08-2023.
-//
+#include <iostream>
+
+class Complex {
+public:
+  int real, imaginary;
+  Complex operator+(Complex c) const {
+    Complex temp{};
+    temp.real = real + c.real;
+    temp.imaginary = imaginary + c.imaginary;
+    return temp;
+  }
+
+  friend std::ostream &operator<<(std::ostream &, Complex);
+};
+
+std::ostream &operator<<(std::ostream &os, Complex c) {
+  os << c.real << " " << c.imaginary;
+  return os;
+}
+
+int main() {
+
+  Complex a = {5, 6}, b = {3, 4}, c{};
+
+  c = a + b;
+  std::cout << c;
+  return EXIT_SUCCESS;
+}
