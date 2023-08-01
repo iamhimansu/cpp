a5f16ae10372701b39c9bae8158d698fdc93d95f
Himanshu Raj Aman <himanshu.aman@samarth.ac.in>
2023-08-01 13:40:56 +0530

    Complex number unary operation

---
+++ friendFunctionUnaryOperatorOverloading.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+class Complex {
+public:
+  int real{}, imaginary{};
+
+private:
+  friend Complex operator-(Complex);
+};
+
+Complex operator-(Complex c) {
+  Complex t;
+  t.real = -c.real;
+  t.imaginary = -c.imaginary;
+  return t;
+}
+
+int main() {
+  Complex a = {1, -2}, b{};
+
+  b = -a;
+
+  std::cout << a.real << " " << a.imaginary << std::endl;
+  std::cout << b.real << " " << b.imaginary << std::endl;
+}
