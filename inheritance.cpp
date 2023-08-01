14016c4fa70e4f7542a42561e14d2a6718ccc2fa
Himanshu Raj Aman <himanshu.aman@samarth.ac.in>
2023-08-01 16:25:58 +0530

    Inheritance comment explaination

--- inheritance.cpp
+++ inheritance.cpp
@@ -2,7 +2,7 @@
 
 class A {
 public:
-  int a, b;
+  int a{}, b{};
   void coutPublic() { std::cout << "Hello Class A" << std::endl; }
 
 private:
@@ -10,16 +10,35 @@ private:
 
 public:
   void getCoutPrivate() { return coutPrivate(); }
+
+protected:
+  std::string s = "Hello I am protected!";
+  std::string getCoutProtected() { return s; }
 };
 
-class B : public A {
+/**
+ * using public keyword will inherit all the properties and methods with their
+ * default access levels
+ *  - Example using public :
+ *      - public -> public
+ *      - private -> private
+ *      - protected -> protected
+ *  - Example using private:
+ *      - public -> private
+ *      - private -> private
+ *      - protected -> private
+ *  - Example using protected:
+ *      - public -> protected
+ *      - private -> private
+ *      - protected -> protected
+ */
+class B : private A {
 public:
-  void inheritCoutPublic() { return coutPublic(); }
+  std::string inheritCoutPublic() { return getCoutProtected(); }
 };
 int main() {
   A a;
   B b;
   a.coutPublic();
-  b.inheritCoutPublic();
-  b.getCoutPrivate();
+  std::cout << b.inheritCoutPublic() << std::endl;
 }
