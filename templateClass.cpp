2a02e0d754a464000ebea2eb17d5a27c793c53a5
Himanshu Raj Aman <himanshu.aman@samarth.ac.in>
2023-08-04 12:17:46 +0530

    Modified struct pointer allocator to Object

--- templateClass.cpp
+++ templateClass.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <cstring>
+
 template<class X>
 class MyStack {
 private:
@@ -7,25 +8,26 @@ private:
     struct Allocation {
         X *arr_ptr{nullptr};
     };
-    Allocation *Allocator{nullptr};
+    Allocation Allocator{};
 
 public:
     explicit MyStack(int size) {
         this->sizeOfStack = size;
-        this->Allocator = new Allocation;
-        this->Allocator->arr_ptr = new X[this->sizeOfStack];
+        Allocation a = {};
+        this->Allocator = a;
+        this->Allocator.arr_ptr = new X[this->sizeOfStack];
     }
 
     void push(X data) {
         if (this->currentIndex < this->sizeOfStack) {
-            this->Allocator->arr_ptr[this->currentIndex] = data;
+            this->Allocator.arr_ptr[this->currentIndex] = data;
             this->currentIndex++;
         }
     }
 
     void list() {
         for (int i = 0; i < this->sizeOfStack; ++i) {
-            std::cout << "index (" << i << "): " << this->Allocator->arr_ptr[i] << std::endl;
+            std::cout << "index (" << i << "): " << this->Allocator.arr_ptr[i] << std::endl;
         }
     }
 
@@ -39,12 +41,12 @@ public:
     X pop() {
         const int sizeOfAllocator = this->sizeOfStack--;
         if (sizeOfAllocator >= 1) {
-            const X poppedValue = this->Allocator->arr_ptr[0];
+            const X poppedValue = this->Allocator.arr_ptr[0];
             for (int i = 0; i < sizeOfAllocator; ++i) {
-                this->Allocator->arr_ptr[i] = this->Allocator->arr_ptr[i + 1];
+                this->Allocator.arr_ptr[i] = this->Allocator.arr_ptr[i + 1];
             }
             X *newAllocation = new X[sizeOfAllocator];
-            memcpy(newAllocation, this->Allocator->arr_ptr, sizeOfAllocator);
+            memcpy(newAllocation, this->Allocator.arr_ptr, sizeOfAllocator);
 //            memmove(newAllocation, this->Allocator->arr_ptr, sizeOfAllocator);
             return poppedValue;
         }
@@ -52,11 +54,11 @@ public:
     }
 
     X top() {
-        return this->Allocator->arr_ptr[0];
+        return this->Allocator.arr_ptr[0];
     }
 
     X bottom() {
-        return this->Allocator->arr_ptr[this->sizeOfStack];
+        return this->Allocator.arr_ptr[this->sizeOfStack];
     }
 
     [[nodiscard]] bool empty() {
@@ -64,14 +66,12 @@ public:
     }
 
     ~MyStack() {
-        delete[]this->Allocator->arr_ptr;
-        delete this->Allocator;
+        delete[]this->Allocator.arr_ptr;
     }
 };
 
 int main() {
     MyStack<float> stack(5);
-    float f{0};
     stack.push(1.4);
     stack.push(2.6);
     stack.push(3.8);
@@ -79,9 +79,18 @@ int main() {
     stack.push(5.2);
     std::cout << "Stack size is: " << stack.size() << std::endl;
 
-    while (!stack.empty()){
+    MyStack<int> stack1(2);
+    stack1.push(1);
+    stack1.push(2);
+
+    while (!stack.empty()) {
         std::cout << "top: " << stack.top() << std::endl;
         stack.pop();
+    }
+    std::cout << std::endl;
+    while (!stack1.empty()) {
+        std::cout << "top: " << stack1.top() << std::endl;
+        stack1.pop();
     }
 //    stack.list();
     return EXIT_SUCCESS;
